aa731bfd7a23568cf51dde8a65101130e51b471d
dzban <59514829+dzban@users.noreply.github.com>
2020-03-28 22:33:18 +0100

    Add files via upload

---
+++ Grounded.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    GameObject Player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = gameObject.transform.parent.gameObject; //tu trzeba zrobic obiekt jako dziecko playera- groundcheck
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) //poczatek kolizji
+    {
+        if (collision.collider.tag == "Ground") //sprawdza czy jest ground
+        {
+            Player.GetComponent<Movement2D>().isGrounded = true;
+
+        }
+    }
+    private void OnCollisionExit2D(Collision2D collision) //koniec kolizji
+    {
+        if (collision.collider.tag == "Ground") //tu tez sprawdza  
+        {
+            Player.GetComponent<Movement2D>().isGrounded = false;
+        }
+    }
+}
---
+++ Movement2D.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement2D : MonoBehaviour
+{
+    public bool isGrounded = false;
+    public float MovementSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Jump();
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f); //getaxis bierze strzalki prawo i lewo i robi z tego wektor poruszania sie 
+        transform.position += movement * Time.deltaTime * MovementSpeed; // przesuniecie
+
+    }
+    void Jump() {
+
+        if (Input.GetButtonDown("Jump")&& isGrounded==true)
+            gameObject.GetComponent<RigidBody2D>().AddForce(newVector2(0f, 5f), ForceMode2D.Impulse); //dodaje sile do rigidbody
+    
+    }
+}
